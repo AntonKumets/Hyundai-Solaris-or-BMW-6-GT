affd4a1e68e89f780abcffd958867b17a5bad124
AntonKumets <kumetsanton@gmail.com>
2025-03-22 18:17:58 +0200

    Update logic_ai.py

--- logic_ai.py
+++ logic_ai.py
@@ -5,7 +5,7 @@ import numpy as np
 def get_class(image_path):
     np.set_printoptions(suppress=True)
     model = load_model("keras_model.h5", compile=False)
-    class_names = open("labels.txt", "r").readlines()
+    class_names = open("labels.txt", "r", encoding = "utf-8").readlines()
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     image = Image.open(image_path).convert("RGB")
     size = (224, 224)
